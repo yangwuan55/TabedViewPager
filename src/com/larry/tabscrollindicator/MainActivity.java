4bcfc9ec93f71c2f8850a68e9dd3f8b9d16e1a6b
yangmengrong <yangmengrong@baiyi-mobile.com>
2014-10-15 14:14:40 +0800

    first init

--- src/com/larry/tabscrollindicator/MainActivity.java
+++ src/com/larry/tabscrollindicator/MainActivity.java
@@ -3,6 +3,7 @@ package com.larry.tabscrollindicator;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.Gravity;
 import android.view.View;
@@ -13,21 +14,27 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
 	private static final String[] ITEMS = new String[] { "Home", "Paper", "Setting" };
 
+    private static final String[] CONTENT = new String[] { "Calendar", "Camera", "Alarms", "Location" };
+    private static final int[] ICONS = new int[] {
+            R.drawable.perm_group_calendar,
+            R.drawable.perm_group_camera,
+            R.drawable.perm_group_device_alarms,
+            R.drawable.perm_group_location,
+    };
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		ViewPagerWithTab view = new ViewPagerWithTab(this);
-		view.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
-		view.setAdapter(new SimplePagerAdapter());
-		setContentView(view);
+        setContentView(R.layout.activity_main);
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+
+		pager.setAdapter(new SimplePagerAdapter());
+
+        TabPageIndicator indicator = (TabPageIndicator)findViewById(R.id.indicator);
+        indicator.setViewPager(pager);
 	}
 	
-	public class SimplePagerAdapter extends TabPagerAdapter {
-
-		@Override
-		public int getCount() {
-			return ITEMS.length;
-		}
+	public class SimplePagerAdapter extends PagerAdapter implements IconPagerAdapter {
 
 		@Override
 		public Object instantiateItem(ViewGroup container, int position) {
@@ -50,14 +57,21 @@ public class MainActivity extends Activity {
 			((ViewPager) container).removeView((View) view);
 		}
 
-		@Override
-		View getTitleView(int position) {
-			TextView view = new TextView(MainActivity.this);
-			view.setGravity(Gravity.CENTER);
-			view.setText(ITEMS[position]);
-			return view;
-		}
-		
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return CONTENT[position % CONTENT.length].toUpperCase();
+        }
+
+        @Override
+        public int getIconResId(int index) {
+            return ICONS[index];
+        }
+
+        @Override
+        public int getCount() {
+            return CONTENT.length;
+        }
+
 	}
 
 }
